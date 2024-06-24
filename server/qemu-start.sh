7f1dc1424e2f0efe83564cf8f429a1728607fac9
Stanislav Ivanov <seivanov@avito.ru>
2024-06-24 22:36:14 +0300

    fix

---
+++ server/qemu-start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#qemu-system-x86_64 -hda ubuntu.qcow -m 8048 -vnc :1 -device e1000,netdev=net0 -netdev user,id=net0,hostfwd=tcp::5555-:22,hostfwd=tcp::55601-:5601 -smp cores=3,threads=1,sockets=4 -nographic &
+qemu-system-x86_64 -hda ubuntu.qcow -m 8048 -device e1000,netdev=net0 -net bridge,br=virbr0 -netdev user,id=net0,hostfwd=tcp::5555-:22,hostfwd=tcp::55601-:5601 -smp cores=2,threads=1,sockets=4 -nographic &
---
+++ server/server-start.sh
@@ -0,0 +1,2 @@
+apt-get update
+apt-get install qemu-system-x86 docker-compose docker-compose-v2 file screen mc libvirt-clients libvirt-daemon libvirtd qemu qemu-kvm libvirt-clients libvirt-daemon-system virtinst bridge-utils
